fff0866f92ef729b3fc5b5a17e0fa0f837a1dd3b
RedSmartRobot <95497514+RedSmartRobot@users.noreply.github.com>
2021-12-09 15:06:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-/* TODO: Add Moralis init code */
+/* Moralis init code */
+const serverUrl = "https://qslrk6lseysn.usemoralis.com:2053/server";
+const appId = "ZdCytbEL6on3zhkrz37GHEAXIinBmEeaRkVRt0LE";
+Moralis.start({ serverUrl, appId });
+
+/* Authentication code */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+      .then(function (user) {
+        console.log("logged in user:", user);
+        console.log(user.get("ethAddress"));
+      })
+      .catch(function (error) {
+        console(error);
+      });
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
